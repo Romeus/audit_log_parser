c5545c08394f22051c2c44125a6f76bbf187863e
Roman Storozhenko <romeusmeister@gmail.com>
2017-08-20 13:21:21 +0300

    Implement sequential server listening localhost on port 8888

--- audit_log_parser.c
+++ audit_log_parser.c
@@ -1,45 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <libaudit.h>
-#include <auparse.h>
+#include <string.h>
 #include <errno.h>
+#include <libaudit.h>
+#include <unistd.h>
+#include <auparse.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define BUFFLEN 524288
+
+static char temporary_buffer[BUFFLEN];
+
+char* fetch_next_event(auparse_state_t* au, char* buf) {
+	do {
+		*buf = '\0';
+		const char* record = auparse_get_record_text(au);
+		if(record == NULL)
+			continue;
+
+		strcat(buf, record);
+	} while (auparse_next_record(au) > 0);
+
+	return buf;
+}
 
 int main(int argc, char *argv[])
 {
 	int err;
+	int server_fd, client_fd;
+	int sock_len;
         auparse_state_t *au;
+	struct sockaddr_in server, client;
 
-	au = auparse_init(AUSOURCE_LOGS, NULL);
-	if (au == NULL) {
-		printf("You should run that program with the root privelegies\n");
-		printf("The error is: %s\n", strerror(errno));
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1) {
+		printf("Couldn't create a socket");
 		exit(1);
 	}
 
-        err = auparse_first_record(au);
-	if (err == -1) {
-		printf("%s", "Couldn't initialize auparse");
+	memset(&server, 0, sizeof(server));
+
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_port = htons(8888);
+
+	if(bind(server_fd,(struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		perror("Couldn't bind to the socket");
+		exit(1);
 	}
 
-        do {
-                do {
-                        char buf[32];
-                        const char *type = auparse_get_type_name(au);
-                        if (type == NULL) {
-                                snprintf(buf, sizeof(buf), "%d",
-                                        auparse_get_type(au));
-                                type = buf;
-                        }
-                        printf("Record type: %s - ", type);
-                        do {
-                                const char *name = auparse_get_field_name(au);
-                                printf("%s,", name);
-                        } while (auparse_next_field(au) > 0);
-                        printf("\b \n");
-                } while (auparse_next_record(au) > 0);
-        } while (auparse_next_event(au) > 0);
+	for(;;) {
+		listen(server_fd, 1);
 
-        auparse_destroy(au);
+		sock_len = sizeof(struct sockaddr_in);
+
+		client_fd = accept(server_fd, (struct sockaddr *)&client, (socklen_t*)&sock_len);
+		if (client_fd > 0)
+		{
+			au = auparse_init(AUSOURCE_LOGS, NULL);
+			if (au == NULL) {
+				perror("You should run that program with the root privelegies\n");
+				exit(1);
+			}
+
+			err = auparse_first_record(au);
+			if (err == -1) {
+				perror("Couldn't initialize auparse");
+			}
+
+			do {
+				fetch_next_event(au, temporary_buffer);
+				send(client_fd, temporary_buffer, strlen(temporary_buffer), 0);
+			} while (auparse_next_event(au) > 0);
+
+			auparse_destroy(au);
+
+		} else {
+		    perror("accept failed");
+		}
+
+		close(client_fd);
+	}
+
+	close(server_fd);
 
         return 0;
 }
